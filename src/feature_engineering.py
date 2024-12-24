f9853009630122d73b4d04e32c80c997c40b3102
Owner <po879r2a@s.okayama-u.ac.jp>
2024-12-24 15:06:14 +0900

    feat:add feature_engineering

---
+++ src/feature_engineering.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import jpholiday
+df = pd.read_csv(r"..\data\df_preprocessed.csv")
+def feature_engineering(df):
+    # Date列をdatetime型に変換
+    df['Date'] = pd.to_datetime(df['Date'])
+    
+    # 祝日フラグの作成
+    df['Is_Holiday'] = df['Date'].apply(lambda x: 1 if jpholiday.is_holiday(x) else 0)
+    
+    # 試合数の作成（同年内で何試合目か）
+    df['Match_Number'] = df.groupby('Year').cumcount() + 1
+    
+    # コロナ期間の削除（2020年2月29日～2022年3月24日）
+    df['Is_Covid_Restricted'] = df['Date'].apply(lambda x: 1 if pd.Timestamp('2020-02-29') <= x <= pd.Timestamp('2022-03-24') else 0)
+    df = df[df['Is_Covid_Restricted'] == 0].drop(columns=['Is_Covid_Restricted'])  # コロナ期間のデータ削除
+    
+    # 雨量0のフラグ
+    df['Rain_Zero_Flag'] = df['Total_Precipitation (mm)'].apply(lambda x: 1 if x == 0 else 0)
+    
+    return df
+df_featured = feature_engineering(df)
+df_featured.to_csv(r"..\data\df_featured.csv",index=False)
